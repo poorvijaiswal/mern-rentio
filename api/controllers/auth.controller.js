90244e4b19c2bc6e5868a624fa1bc72d5fc5711a
Sneha-Porwal <porwalsneha5@gmail.com>
2024-10-10 13:47:16 +0530

    Adding files

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+import User from '../modules/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const singup =async(req,res,next) => {
+    const {username,emai,password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password,10);
+    const newUser = new User({ username,email,password,hashedPassword});
+    try{
+        await newUser.save();
+        res.status(201).json('User created successfully!');
+
+    }catch (error) {
+        next(error);
+    }
+};
+
+export const singin =async(req,res,next) => {
+    const {email,password} = req.body;
+    try {
+        const validUser = await User.findOne ({ email});
+        if (!validUser)  return next(errorHandler(404,'User not found'));
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401,'Wrong credentials'));
+
+    }catch(error){
+        next(error);
+    }
+}
--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -2,5 +2,5 @@ import express from 'express'
 import {singup} from '../controllers/auth.controller.js';
 
 const router = express.Router();
-router.post("singup",singup);
+router.post("/singup",singup);
 export default router;
