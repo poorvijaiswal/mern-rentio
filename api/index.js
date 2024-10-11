421eb32cc0356a1676c74881b4492dc0cd721da8
Sneha-Porwal <porwalsneha5@gmail.com>
2024-10-11 19:48:22 +0530

    modify the files

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
-
+import authRouter from './routes/authRouter.route.js';
 const app = express();
-
-app.listen(4000, () => {
+app.use(epxress.jason());
+app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000!!!');
+});
+
+app.use('/api/auth',authRouter);
+app.use((err,req,res,next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message  || 'Internal Server Error';
+  return res.status(statusCode),json({
+    sucess:false,
+    statusCode,
+    message,
+  });
 });
