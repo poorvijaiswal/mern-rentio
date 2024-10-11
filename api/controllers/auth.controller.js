e97514421ad4f710f5d0c236eddd4c38be7f6b97
Sneha-Porwal <porwalsneha5@gmail.com>
2024-10-11 20:10:17 +0530

    adding contents

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../modules/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const singup =async(req,res,next) => {
     const {username,emai,password } = req.body;
@@ -21,7 +22,7 @@ export const singin =async(req,res,next) => {
         if (!validUser)  return next(errorHandler(404,'User not found'));
         const validPassword = bcryptjs.compareSync(password,validUser.password);
         if(!validPassword) return next(errorHandler(401,'Wrong credentials'));
-
+        const token = jwt.sign({ id: validUser._id},process.env.JWT_SECRET)
     }catch(error){
         next(error);
     }
