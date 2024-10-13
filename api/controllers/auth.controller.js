a29451f68f12ef6b946c2443110c5e269e46d1b8
poorvijaiswal <jaiswalpoorvii0806@gmail.com>
2024-10-13 19:45:20 +0530

    resolved errors of branch sneha

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,29 +1,29 @@
-import User from '../modules/user.model.js';
+import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-export const singup =async(req,res,next) => {
-    const {username,emai,password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password,10);
-    const newUser = new User({ username,email,password,hashedPassword});
-    try{
+export const singup = async (req, res, next) => {
+    const { username, emai, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password, hashedPassword });
+    try {
         await newUser.save();
         res.status(201).json('User created successfully!');
 
-    }catch (error) {
+    } catch (error) {
         next(error);
     }
 };
 
-export const singin =async(req,res,next) => {
-    const {email,password} = req.body;
+export const singin = async (req, res, next) => {
+    const { email, password } = req.body;
     try {
-        const validUser = await User.findOne ({ email});
-        if (!validUser)  return next(errorHandler(404,'User not found'));
-        const validPassword = bcryptjs.compareSync(password,validUser.password);
-        if(!validPassword) return next(errorHandler(401,'Wrong credentials'));
-        const token = jwt.sign({ id: validUser._id},process.env.JWT_SECRET)
-    }catch(error){
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    } catch (error) {
         next(error);
     }
 }
--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-import authRouter from './routes/authRouter.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,19 +13,19 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
-app.use(epxress.json());
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000!!!');
 });
 
 app.use('/api/user', userRouter);
-app.use('/api/auth',authRouter);
-app.use((err,req,res,next) => {
+app.use('/api/auth', authRouter);
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message  || 'Internal Server Error';
-  return res.status(statusCode),json({
-    sucess:false,
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode), json({
+    sucess: false,
     statusCode,
     message,
   });
