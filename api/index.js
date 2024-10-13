a8fa5f5a1c0087151fdb5a2497c4792c92a8f0f4
poorvijaiswal <jaiswalpoorvii0806@gmail.com>
2024-10-13 17:52:10 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Successfully Connected to MongoDB");
+}).catch((err) => {
+  console.log(err);
+});
 
 const app = express();
 
-app.listen(4000, () => {
+app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000!!!');
 });
