f6e51a69ba57f7e9570a67ea11cbdf3d58adc749
Sneha-Porwal <142002344+Sneha-Porwal@users.noreply.github.com>
2024-10-13 20:35:24 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-import authRouter from './routes/authRouter.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
-app.use(epxress.json());
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000!!!');
