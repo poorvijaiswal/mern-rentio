4108d7da0c4aa3937a1d9b928c65d13252131f04
Sneha-Porwal <porwalsneha5@gmail.com>
2024-10-14 22:40:27 +0530

    signin api completed

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -15,7 +15,7 @@ export default function SignUp() {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('/api/auth/singup', {
+      const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
