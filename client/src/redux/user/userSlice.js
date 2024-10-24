212a9d1d3a6406d78a4412b257fcb95eeb716a21
Sneha-Porwal <porwalsneha5@gmail.com>
2024-10-24 09:01:57 +0530

    updation done

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -21,7 +21,19 @@ const userSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
       },
+      updateUserStart: (state) => {
+        state.loading = true;
+      },
+      updateUserSuccess: (state, action) => {
+        state.currentUser = action.payload;
+        state.loading = false;
+        state.error = null;
+      },
+      updateUserFailure: (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+      },
     }
   });
-  export const {signInStart,signInSuccess,signInFailure} = userSlice.actions;
+  export const {signInStart, signInSuccess, signInFailure, updateUserFailure, updateUserSuccess, updateUserStart,} = userSlice.actions;
   export default userSlice.reducer;
