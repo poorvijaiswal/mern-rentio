8baa1f85256a418d986e8fa9252d334584687100
Sneha-Porwal <porwalsneha5@gmail.com>
2024-10-24 07:57:32 +0530

    update user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,37 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+
 export const test = (req, res) => {
     res.json({
         message: "hey! it's working!",
     });
 };
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id)
+      return next(errorHandler(401, 'You can only update your own account!'));
+    try {
+      if (req.body.password) {
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+      }
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: {                   //set check if the data is being changed is going to change otherwise going to ignore the data
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            avatar: req.body.avatar,
+          },
+        }, { new: true }
+      );
+  
+      const { password, ...rest } = updatedUser._doc;
+  
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
+  
--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -16,6 +15,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000!!!');
